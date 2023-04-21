5340ae38f0e30551d8aea7a04b32f1a259480819
CodingCat <450113828@qq.com>
2023-04-21 21:30:50 +0800

    渲染

--- todos/src/App.vue
+++ todos/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <todoHeader></todoHeader>
+        <todoMain></todoMain>
+        <todoFooter></todoFooter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import todoHeader from "./components/todoHeader.vue";
+import todoMain from "./components/todoMain.vue";
+import todoFooter from "./components/todoFooter.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    todoHeader,
+    todoMain,
+    todoFooter,
   },
 };
 </script>
@@ -24,5 +32,50 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
---
+++ todos/src/components/todoMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul class="todo-main">
+      <TodoItem
+        v-for="todoitm in todos"
+        :key="todoitm.id"
+        :todo="todoitm"
+      ></TodoItem>
+    </ul>
+  </div>
+</template>
+<script>
+import TodoItem from "./todoItem.vue";
+
+export default {
+  name: "todoMain",
+  components: { TodoItem },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "唱歌", completed: true },
+        { id: "002", title: "打游戏", completed: false },
+        { id: "003", title: "编程", completed: true },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
